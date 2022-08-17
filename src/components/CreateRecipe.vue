a5e7c04c3231567243075f3a7dfc052b802307fd
Dizmonian <doram2013@gmail.com>
2022-08-17 10:16:02 +0300

    Created "about" page and updated CreateRecipe

--- src/components/CreateRecipe.vue
+++ src/components/CreateRecipe.vue
@@ -1,154 +1,143 @@
 <template>
     <div class="container">
-    <!-- <h1 class="title-text">Create New Recipe</h1> -->
-
-
 
     <div>
-  <!-- Using modifiers -->
-<div class="w-30 ">
-  <b-button text-decoration-none class="nav-link" variant="link" v-b-modal.createRecipe style="text-decoration: none;">Create Recipe</b-button>
-</div>
-  <!-- <a class="nav-link" href="" v-b-modal.createRecipe>Create Recipe</a> -->
-
-
-  <!-- Using value -->
-  <!-- The modal -->
-  <b-modal 
-    id="createRecipe" 
-    ref="modal"
-    title="Create Your Own Recipe!"
-    @ok="handleOk"
-    @show="resetModal"
-    @hidden="resetModal"
-    >
-    
-    <b-form @submit.prevent="onSubmit">
-    <!--Not onSubmit - on the method name. probably the one to register to db-->
-      <b-form-group
-        id="input-group-Recipe Name"
-        label="RecipeName:"
-        label-for="title"
-        :state="form.titleState"
+  <div class="w-30 ">
+    <b-button text-decoration-none class="nav-link" variant="link" v-b-modal.createRecipe style="text-decoration: none;">Create Recipe</b-button>
+  </div>
+    <b-modal 
+      id="createRecipe" 
+      ref="modal"
+      title="Create Your Own Recipe!"
+      @ok="handleOk"
+      @show="resetModal"
+      @hidden="resetModal"
       >
-        <b-form-input required
-          class="inputs"
-          id="title"
-          type="text"
-          v-model="form.title"
-          :state="form.titleState"
-        ></b-form-input>
-        <b-form-invalid-feedback>
-          RecipeName is required  
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <b-form-group
-        id="input-group-ImageURL"
-        label="Image URL:"
-        label-for="ImageURL"
-        :state="form.ImageURLState"
-      >
-        <b-form-input
-          class="inputs"
-          id="ImageURL"
-          type="url"
-          v-model="form.ImageURL"
-          :state="form.ImageURLState"
-        ></b-form-input>
-        <b-form-invalid-feedback>
-          ImageURL is required
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <b-form-group
-        id="input-group-TotalTime"
-        label="Preperation Time:"
-        label-for="TotalTime"
-        :state="form.TotalTimeState"
-      >
-      <b-form-input
-          class="inputs"
-          id="TotalTime"
-          type="number"
-          v-model="form.TotalTime"
-          :state="form.TotalTimeState"
-        ></b-form-input>
-        <b-form-invalid-feedback>
-          Preperation Time is required  
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <b-form-group
-        id="input-group-Ingredients"
-        label="Ingredients List and Amounts.
-        (Use linebreak for each ingredients):"
-        label-for="Ingredients"
-        :state="form.IngredientsState"
-      >
-        <b-form-textarea
-          class="inputs"
-          id="Ingredients"
-          v-model="form.Ingredients"
-          :state="form.IngredientsState"
-        ></b-form-textarea>
-          <!-- <pre class="mt-3 mb-0">{{ Ingredients }}</pre> -->
-        <b-form-invalid-feedback>
-          Ingredients List and Amounts are required  
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <b-form-group
-        id="input-group-instructions"
-        label="Instructions
-        (Use linebreak for each stage):"
-        label-for="instructions"
-        :state="form.instructionsState"
-
-      >
-        <b-form-textarea
-          class="inputs"
-          id="instructions"
-          v-model="form.instructions"
-          :state="form.instructionsState"
-        ></b-form-textarea>
-        <b-form-invalid-feedback>
-          Instructions is required 
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <b-form-group
-        id="input-group-dishNum"
-        label="Number of Dishes:"
-        label-for="dishNum"
-        :state="form.dishNumState"                  
-
-      >
-        <b-form-input
-          class="inputs"
-          id="dishNum"
-          type="number"
-          v-model="form.dishNum"
-          :state="form.dishNumState"                  
-        ></b-form-input>
-        <b-form-invalid-feedback>
-          Number of Dishes is required
-        </b-form-invalid-feedback>
-      </b-form-group>
-
-      <input type="checkbox" v-model="form.vegan" id="vegan">
-      <label for="vegan">  Vegan</label>
-      <br>
-      <input type="checkbox" v-model="form.vegetarian" id="vegetarian">
-      <label for="vegetarian"> Vegetarian</label>
-      <br>
-      <input type="checkbox" id="glutenFree" v-model="form.glutenFree">
-      <label for="glutenFree"> Gluten Free </label>
-
-
       
-    </b-form>
-  </b-modal>
+      <b-form @submit.prevent="onSubmit">
+        <b-form-group
+          id="input-group-Recipe Name"
+          label="RecipeName:"
+          label-for="title"
+          :state="form.titleState"
+        >
+          <b-form-input required
+            class="inputs"
+            id="title"
+            type="text"
+            v-model="form.title"
+            :state="form.titleState"
+          ></b-form-input>
+          <b-form-invalid-feedback>
+            RecipeName is required  
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-ImageURL"
+          label="Image URL:"
+          label-for="ImageURL"
+          :state="form.ImageURLState"
+        >
+          <b-form-input
+            class="inputs"
+            id="ImageURL"
+            type="url"
+            v-model="form.ImageURL"
+            :state="form.ImageURLState"
+          ></b-form-input>
+          <b-form-invalid-feedback>
+            Please insert a valid URL
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-TotalTime"
+          label="Preperation Time:"
+          label-for="TotalTime"
+          :state="form.TotalTimeState"
+        >
+        <b-form-input
+            class="inputs"
+            id="TotalTime"
+            type="number"
+            v-model="form.TotalTime"
+            :state="form.TotalTimeState"
+          ></b-form-input>
+          <b-form-invalid-feedback>
+            Preperation Time is required  
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-Ingredients"
+          label="Ingredients List and Amounts.
+          (Use linebreak for each ingredients):"
+          label-for="Ingredients"
+          :state="form.IngredientsState"
+        >
+          <b-form-textarea
+            class="inputs"
+            id="Ingredients"
+            v-model="form.Ingredients"
+            :state="form.IngredientsState"
+          ></b-form-textarea>
+          <b-form-invalid-feedback>
+            Ingredients List and Amounts are required  
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-instructions"
+          label="Instructions
+          (Use linebreak for each stage):"
+          label-for="instructions"
+          :state="form.instructionsState"
+
+        >
+          <b-form-textarea
+            class="inputs"
+            id="instructions"
+            v-model="form.instructions"
+            :state="form.instructionsState"
+          ></b-form-textarea>
+          <b-form-invalid-feedback>
+            Instructions is required 
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-dishNum"
+          label="Number of Dishes:"
+          label-for="dishNum"
+          :state="form.dishNumState"                  
+
+        >
+          <b-form-input
+            class="inputs"
+            id="dishNum"
+            type="number"
+            v-model="form.dishNum"
+            :state="form.dishNumState"                  
+          ></b-form-input>
+          <b-form-invalid-feedback>
+            Number of Dishes is required
+          </b-form-invalid-feedback>
+        </b-form-group>
+
+        <input type="checkbox" v-model="form.vegan" id="vegan">
+        <label for="vegan">  Vegan</label>
+        <br>
+        <input type="checkbox" v-model="form.vegetarian" id="vegetarian">
+        <label for="vegetarian"> Vegetarian</label>
+        <br>
+        <input type="checkbox" id="glutenFree" v-model="form.glutenFree">
+        <label for="glutenFree"> Gluten Free </label>
+
+
+        
+      </b-form>
+    </b-modal>
 </div>
     </div>  
 
@@ -161,7 +150,6 @@ import { BButton, BModal, VBModal } from "bootstrap-vue";
     export default {
       name: "CreateRecipe",
       components: {
-          // BButton,
           BModal
       },
       data(){
@@ -191,26 +179,12 @@ import { BButton, BModal, VBModal } from "bootstrap-vue";
       
       methods:{
         handleOk(bvModalEvent) {
-          // Prevent modal from closing
-          bvModalEvent.preventDefault()
-          // Trigger submit handler
           if (!this.checkFormValidity()) {
+            bvModalEvent.preventDefault()
             return
           }
           this.onSubmit()
         },      
-        
-        handleSubmit() {
-          // Exit when the form isn't valid
-          if (!this.checkFormValidity()) {
-            return
-          }
-          // Push the name to submitted names
-          // Hide the modal manually
-          // this.$nextTick(() => {
-          //   this.$bvModal.hide('modal-prevent-closing')
-          // })
-        },
 
         checkFormValidity() {
           if (this.form.title.length == 0){
@@ -221,10 +195,22 @@ import { BButton, BModal, VBModal } from "bootstrap-vue";
             {
               this.form.titleState = true
             }
-          if (this.form.ImageURL.length == 0){ //Might need to check if can verify valid URL.
-            this.form.ImageURLState = false
+          let url;
+          try{
+            url = new URL(this.form.ImageURL);
+          }catch(error){
+            this.form.ImageURLState = false 
             return false
+          }
+          if(url.protocol === "http:" || url.protocol === "https:"){
+            if (this.form.ImageURL.length == 0){ 
+              this.form.ImageURLState = false
+              return false
+              }
+            else{
+            this.form.ImageURLState = true
             }
+          }
           else{
             this.form.ImageURLState = true
           }
@@ -294,16 +280,6 @@ import { BButton, BModal, VBModal } from "bootstrap-vue";
         async onSubmit(){
           try{
            
-            console.log(this.form.ImageURL)
-            console.log(this.form.title)
-            console.log(this.form.TotalTime)
-            console.log(this.form.vegan)
-            console.log(this.form.vegetarian)
-            console.log(this.form.glutenFree)
-            console.log(this.form.Ingredients)
-            console.log(this.form.instructions)
-            console.log(this.form.dishNum)
-
             const request = await this.axios.post(
             "http://localhost:80/user/createRecipe",       
             {
@@ -321,7 +297,6 @@ import { BButton, BModal, VBModal } from "bootstrap-vue";
           )
           alert("recipe submitted successfuly!")
           this.resetModal()
-          this.$refs[createRecipe].hide()
           }catch(err){
             alert("An error has accured, Please try again.")
           }
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <h1 align="center">About Us! 😃</h1>
+    <!-- <img src="../src/Photos/JavaScript-Logo.png"> -->
+    <!-- <img src="src\Photos\NodeJS.png"> -->
+    <h1 align="center"><B>About Us! 😃</B></h1>
 
-    <pre align="center">
-        We are two Information Systems Engineers students in the end of our 3rd year.
-        
-
-
-    </pre>
+    <p align="left">
+    
+    <U>Dor Amsalem</U>:
+    <br>
+    I'm a 3rd year student Information System's Engineer student at Ben Gurion University of the Negev,
+    Currently working as an Information Security Infrastructure Engineer at Intel with high affinity for Information & Network security.
+    Working on this assignment has granted me great knowledge and understanding regarding Javascript, NodeJS - especially using express, API and Vue.
+    <br>
+    Linkedin Profile: <a href="https://www.linkedin.com/in/dor-amsalem-2839ba206/">https://www.linkedin.com/in/dor-amsalem-2839ba206/</a>
+    </p>
+    <p>
+    <U>Alon Sadot</U>:
+    I'm a 3rd year student Information System's Engineer student at Ben Gurion University of the Negev.
+    <br>
+    Linkedin Profile: <a href="https://www.linkedin.com/in/alon-sadot/">https://www.linkedin.com/in/alon-sadot/</a>
+    </p>
 
   </div>
 </template>
@@ -26,9 +38,22 @@ h1{
     font-family: Cursive;
 }
 pre{
-    font-size: 30px;
+    font-size: 25px;
     margin-top: 20px;
 }
-
+p{
+    font-size: 28px;
+    margin-top: 30px;
+    margin-left: 85px;
+}
+body, html {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  min-height: 100vh;
+}
+body {
+  background-color: azure;
+}
 </style>
 
